af9fcc8e7ee5d213bb64ff3ed278bba590cbdab9
vitorfblima <vitorfblima@gmail.com>
2017-01-04 16:48:34 -0300

    updated the bot to speak

--- index.js
+++ index.js
@@ -28,3 +28,39 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAOyvSqO3jsBALEa5WrYy7hhdjmfum9EgxyVUNlhCHfbIR3TSOiPYn7ADZAEVZAkViuoUvTP39xAMX7yjv7ZAnAZA0YRLRTLZCBoKVGwdlpoL7UytNN0sdSaBjZCCTQ5ywhrIae0CZC0EDrA4uTY2QF1AtWFT4WiaFYLRHeYiqRZCQZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.8/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
